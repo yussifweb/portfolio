b413376482b15de2ed5711604470d99cd0a9e2e4
Yussif <djiceman.gh@gmail.com>
2020-09-07 16:03:12 +0000

    Nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Index from './components/Index'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Index></Index>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import info from './info'
+
+function home() {
+    console.log(info);
+    return (
+        <div>
+            <div>{'intro1'}</div>
+            
+        </div>
+    )
+}
+
+export default home
